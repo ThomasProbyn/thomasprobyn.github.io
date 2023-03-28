2bc68754df2bba69bbf80039de1a524b1348e312
ThomasProbyn <tprobyn101@gmail.com>
2023-03-28 15:43:19 +0100

    Added The Classics EP

--- rsc/index/index.js
+++ rsc/index/index.js
@@ -27,5 +27,36 @@ indexObject = {
             },
             "downloadURL" : "./rsc/audio/day-one/day-one-all-songs.zip"
         },
+        {
+            "name" : "The Classics",
+            "image" : "./rsc/img/the-classics/Promo.jpeg",
+            "tracks" : [
+                {
+                    "name" : "By The Light Of The Moon",
+                    "url" : "./rsc/audio/the-classics/btlotm.mp3",
+                },
+                {
+                    "name" : "Purple Cloud",
+                    "url" : "./rsc/audio/the-classics/purple.mp3",
+                },
+                {
+                    "name" : "Travelling",
+                    "url" : "./rsc/audio/the-classics/travelling.mp3",
+                },
+                {
+                    "name" : "Always There",
+                    "url" : "./rsc/audio/the-classics/always.mp3",
+                },
+                {
+                    "name" : "Truth is in your eyes",
+                    "url" : "./rsc/audio/the-classics/eyes.mp3",
+                },
+            ],
+            "data" : {
+                "year" : "2022/2021",
+                "season" : "Released"
+            },
+            "downloadURL" : "./rsc/audio/the-classics/the-classics-all-songs.zip"
+        },
     ]
 }
