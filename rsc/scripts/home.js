9a6d7a3528be20341fb142fae01c98eedc776bd2
ThomasProbyn <tprobyn101@gmail.com>
2023-03-28 11:27:54 +0100

    Made that image clickable

--- rsc/scripts/home.js
+++ rsc/scripts/home.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", (event) => {
     for (i = 0; i < indexObject.albums.length; i++) {
         //for all albums
-        document.getElementById("home-page-promotion-section").innerHTML = document.getElementById("home-page-promotion-section").innerHTML + "<div class=\"album-card\"><img src=\"" + indexObject.albums[i].image + "\" class=\"album-card-image\"><div class=\"primary-album-card-text-elements\" onclick=\"window.location.href = './player.html?album=" + i + "'\"><h1 class=\"primary-text-colour primary-text-font\" id=\"album-main-title-text\">" + indexObject.albums[i].name + "</h1><p class=\"primary-text-colour primary-text-font\" id=\"album-main-release-date\">" + indexObject.albums[i].data.season + " " + indexObject.albums[i].data.year + "</p></div></div>"
+        document.getElementById("home-page-promotion-section").innerHTML = document.getElementById("home-page-promotion-section").innerHTML + "<div class=\"album-card\"><img onclick=\"window.location.href = './player.html?album=" + i + "'\" src=\"" + indexObject.albums[i].image + "\" class=\"album-card-image\"><div class=\"primary-album-card-text-elements\" onclick=\"window.location.href = './player.html?album=" + i + "'\"><h1 class=\"primary-text-colour primary-text-font\" id=\"album-main-title-text\">" + indexObject.albums[i].name + "</h1><p class=\"primary-text-colour primary-text-font\" id=\"album-main-release-date\">" + indexObject.albums[i].data.season + " " + indexObject.albums[i].data.year + "</p></div></div>"
     }
 })
 
